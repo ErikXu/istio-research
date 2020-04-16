a02045ec9b52b7804973c7c7ea4bbd61112e0981
xuzhe <xuzhe@q1.com>
2020-04-16 15:56:56 +0800

    Update istio-entry.

--- istio-entry.sh
+++ istio-entry.sh
@@ -4,6 +4,22 @@ SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -
 
 INGRESS_HOST=$(kubectl get po -l istio=ingressgateway -n istio-system -o jsonpath='{.items[0].status.hostIP}')
 
-echo 'http://'$INGRESS_HOST:$INGRESS_PORT
+KIALI_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="kiali")].nodePort}')
 
-echo 'https://'$INGRESS_HOST:$SECURE_INGRESS_PORT
+PROMETHEUS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="prometheus")].nodePort}')
+
+GRAFANA_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="grafana")].nodePort}')
+
+TRACING_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="tracing")].nodePort}')
+
+echo 'http - http://'$INGRESS_HOST:$INGRESS_PORT
+
+echo 'https - https://'$INGRESS_HOST:$SECURE_INGRESS_PORT
+
+echo 'kiali - http://'$INGRESS_HOST:$KIALI_PORT
+
+echo 'prometheus - http://'$INGRESS_HOST:$PROMETHEUS_PORT
+
+echo 'grafana - http://'$INGRESS_HOST:$GRAFANA_PORT
+
+echo 'tracing - http://'$INGRESS_HOST:$TRACING_PORT
